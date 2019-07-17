53f9572d4c25b6ccdb0d892229b6a4cd56e6906c
rahulbhat31 <rahulbhat1203@yahoo.in>
2019-07-16 21:20:41 -0400

    added and changed some layouts

--- app/src/main/java/com/example/letswakeup/TopicVideoPage.java
+++ app/src/main/java/com/example/letswakeup/TopicVideoPage.java
@@ -2,24 +2,46 @@ package com.example.letswakeup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.MediaController;
 import android.widget.VideoView;
+
+import java.lang.reflect.Method;
 
 public class TopicVideoPage extends AppCompatActivity {
 
+    String SrcPath = "rtsp://v5.cache1.c.youtube.com/CjYLENy73wIaLQnhycnrJQ8qmRMYESARFEIJbXYtZ29vZ2xlSARSBXdhdGNoYPj_hYjnq6uUTQw=/0/0/0/video.3gp";
+
     WebView mWebView;
-    @Override
+    private boolean mIsPaused = false;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topic_video_page);
-        mWebView = findViewById(R.id.topic_video);
-        mWebView.getSettings().setJavaScriptEnabled(true);
-        mWebView.getSettings().setPluginState(WebSettings.PluginState.ON);
-        mWebView.loadUrl("https://www.youtube.com/watch?v=Vkq_srFGW5I");
-        mWebView.setWebChromeClient(new WebChromeClient());
+        String media_url = "https://www.youtube.com/watch?v=B-nEYsyRlYo";
 
+        WebView youtubeWebView = findViewById(R.id.youtube_web_view); //todo find or bind web view
+        String myVideoYoutubeId = "B-nEYsyRlYo";
+
+        youtubeWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                return false;
+            }
+        });
+
+        WebSettings webSettings = youtubeWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        youtubeWebView.loadUrl("https://www.youtube.com/embed/" + myVideoYoutubeId);
     }
+
+
 }
