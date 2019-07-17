a89631bff4e9c19f991cc2341460d641443447dd
Ruchitha Shanmughan <ruchithams1@gmail.com>
2019-07-17 19:35:59 -0400

    lets_wake_up: adding the questionnaire page(working)

---
+++ app/src/main/java/com/example/letswakeup/QuestionsActivity.java
@@ -0,0 +1,60 @@
+package com.example.letswakeup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class QuestionsActivity extends AppCompatActivity {
+
+
+    Button submitBtn;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    String responseText;
+    ImageView rightAns;
+    ImageView wrongAns;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions);
+
+        submitBtn = (Button) findViewById(R.id.submitBtnID);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroupID);
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                radioButton = (RadioButton) findViewById(selectedId);
+                responseText = radioButton.getText().toString();
+
+                if(responseText.equals("Carbon dioxide"))
+                {
+                    wrongAns = (ImageView) findViewById(R.id.wrongAnswerImgID);
+                    wrongAns.setVisibility(View.GONE);
+                    rightAns = (ImageView) findViewById(R.id.rightAnswerImgID);
+                    rightAns.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    rightAns = (ImageView) findViewById(R.id.rightAnswerImgID);
+                    rightAns.setVisibility(View.GONE);
+                    wrongAns = (ImageView) findViewById(R.id.wrongAnswerImgID);
+                    wrongAns.setVisibility(View.VISIBLE);
+                }
+
+
+
+            }
+        });
+
+    }
+}
