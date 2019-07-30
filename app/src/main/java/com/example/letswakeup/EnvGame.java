603e66ef63c72a62b2f2ee5453af564f6f57b7c5
rahulbhat31 <rahulbhat1203@yahoo.in>
2019-07-30 10:49:43 -0400

    added env_game

---
+++ app/src/main/java/com/example/letswakeup/EnvGame.java
@@ -0,0 +1,14 @@
+package com.example.letswakeup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnvGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_env_game);
+    }
+}
